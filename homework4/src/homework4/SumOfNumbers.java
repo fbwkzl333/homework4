772844a68c6077403f37cee09af372cc550641fe
fbwkzl333 <fbwkzl111@naver.com>
2016-10-08 00:22:50 +0900

    메소드 기능 작성(수정중)

--- homework4/src/homework4/SumOfNumbers.java
+++ homework4/src/homework4/SumOfNumbers.java
@@ -1,5 +1,22 @@
+//(1번메뉴) 1부터 입력한 수까지 더하기 
 package homework4;
 
+import java.util.Scanner;
 public class SumOfNumbers {
-
+	Scanner s = new Scanner(System.in);
+	public void SumOf(){
+		System.out.print("\n1부터 입력한 수까지 더하기 마지막 수를 입력(Q:종료) : ");
+		String a = s.nextLine();
+		
+		if(a == "Q"){
+			System.out.println("Goodbye Thank you !");
+		}
+		
+		int result =0;
+		for(int i = 1; i <= Integer.parseInt(a); i++){
+			result = result + i;
+		}
+		System.out.print("총 합은 "+result+"입니다.");
+		SumOf();
+	}
 }
