a6b7ddd058f9022af5c3f7c276bfe388a152c4ae
fbwkzl333 <fbwkzl111@naver.com>
2016-10-08 00:22:04 +0900

    기능 메서드 (수정중)

--- homework4/src/homework4/MaxAndMin.java
+++ homework4/src/homework4/MaxAndMin.java
@@ -1,5 +1,14 @@
+// (2번) 최대값/최소값구하기
 package homework4;
 
+import java.util.Scanner;
 public class MaxAndMin {
-
+	public void MaxMin(){
+		Scanner s = new Scanner(System.in);
+		System.out.print("0~100사이의 숫자를 입력(Q:종료) >>");
+		int a = s.nextInt();
+		if(a == 1){
+			System.out.println("Goodbye Thank you !");
+		}
+	}
 }
