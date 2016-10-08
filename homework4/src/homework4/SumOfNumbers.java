7bc65d9cce50971aecd2cd57acbac7be4ea67093
fbwkzl333 <fbwkzl111@naver.com>
2016-10-09 06:34:16 +0900

    1 클래스 (1부터 입력한 수까지 더하기) 기능 완료

--- homework4/src/homework4/SumOfNumbers.java
+++ homework4/src/homework4/SumOfNumbers.java
@@ -6,17 +6,20 @@ public class SumOfNumbers {
 	Scanner s = new Scanner(System.in);
 	public void SumOf(){
 		System.out.print("\n1부터 입력한 수까지 더하기 마지막 수를 입력(Q:종료) : ");
-		String a = s.nextLine();
+		String a = s.next();
 		
-		if(a == "Q"){
-			System.out.println("Goodbye Thank you !");
+		switch(a){		// 종료 : 메인메뉴로 돌아감
+		case "Q":
+			ExamForWhile.Start();
+			break;
 		}
+		
 		
 		int result =0;
 		for(int i = 1; i <= Integer.parseInt(a); i++){
 			result = result + i;
 		}
 		System.out.print("총 합은 "+result+"입니다.");
-		SumOf();
+		SumOf();	
 	}
 }
