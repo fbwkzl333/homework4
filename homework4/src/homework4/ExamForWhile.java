d0f91ee4bf973e576170472cfc96b4a8d198a06c
fbwkzl333 <fbwkzl111@naver.com>
2016-10-12 12:49:50 +0900

    Main 클래스 1~6번 기능 최종 완료 (+주석)

--- homework4/src/homework4/ExamForWhile.java
+++ homework4/src/homework4/ExamForWhile.java
@@ -5,9 +5,9 @@ public class ExamForWhile {
 
 
 	public static void main(String[] args) {
-				Start();
+				Start();		// 메소드 호출 ( 프로그램 시작 )
 	}
-	public static void Start(){
+	public static void Start(){		// 메뉴 1~6 기능 ( 각 클래스에서 호출함 )
 		System.out.print("1. 1부터 입력한 수까지 더하기\n"
 				+ "2. 최대값/최소값구하기\n"
 				+ "3. 입력받은숫자의 합계와 평균구하기\n"
@@ -16,12 +16,12 @@ public class ExamForWhile {
 				+ "6. 종료하기\n"
 				+ "원하는 메뉴는>>");
 		Scanner s = new Scanner(System.in);
-		int main = s.nextInt();
-		switch(main){
-		case 1:
-			SumOfNumbers num1 = new SumOfNumbers();
-			num1.SumOf();
-			break;
+		int main = s.nextInt();		// Int형 을 입력받아 main에 집어넣음
+		switch(main){	// main 을 비교
+		case 1:	// 입력받은 int main이 1일때
+			SumOfNumbers num1 = new SumOfNumbers();		// 1기능 클래스 인스턴스 생성
+			num1.SumOf();	// 호출
+			break;	// 스위치문 나감
 		case 2:
 			MaxAndMin num2 = new MaxAndMin();
 			num2.MaxMin();
@@ -41,9 +41,9 @@ public class ExamForWhile {
 		case 6:
 			System.out.println("Goodbye Thank you !");
 			break;
-		case 7:		// 테스트용
-			MaxAndMin num7 = new MaxAndMin();
-			num7.Maxmin2();
+		case 7:		// 테스트용 과제와는 관련없음
+			Exception num6 = new Exception();
+			num6.Excep();
 			break;
 		}
 	}
